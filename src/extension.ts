ab194c86722a76b5c5cd2b532e214fa4c116e368
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2021-09-09 16:26:10 +0200

    Added second command

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Extension "dsltoolstojson" is now active!');
 
-	let disposable = vscode.commands.registerCommand('dsltoolstojson.dsltoolstojson', () => {
+	context.subscriptions.push(vscode.commands.registerCommand('dsltoolstojson.dsltoolstojson', () => {
 		// Convert the selection (or if nothing is selected, the entire document) from the output format from the DSLTools, to regular JSON, and then format the document.
 		let editor = vscode.window.activeTextEditor;
 
@@ -14,18 +14,39 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		
 		// Obtain the text to work with.
-		let operationRange = editor!.selection.isEmpty ? GetRangeFullDocument(editor!.document) : new vscode.Range(editor.selection.start, editor.selection.end);
-		let text: string = editor!.document.getText(operationRange) ?? "";
+		let {operationRange, text} = GetTextToWorkWith(editor);
 
 		// Replace with modified version.
 		editor!.edit(editBuilder => editBuilder.replace(operationRange, DSLToolToJSON(text)));
 		
 		// Format the document.
-		vscode.commands.executeCommand("editor.action.formatDocument");
-		vscode.window.showInformationMessage(text);
-	});
+		vscode.commands.executeCommand("editor.action.formatDocument");	
+	}));
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('dsltoolstojson.jsontodsltool', () => {
+		// Convert the selection (or if nothing is selected, the entire document) from the output format from the DSLTools, to regular JSON, and then format the document.
+		let editor = vscode.window.activeTextEditor;
+
+		if (!editor){
+			return;
+		}		
+		
+		// Obtain the text to work with.
+		let {operationRange, text} = GetTextToWorkWith(editor);
+
+		// Replace with modified version.
+		editor!.edit(editBuilder => editBuilder.replace(operationRange, "still working on this one"));	
+	}));
+}
+
+function GetTextToWorkWith(editor: vscode.TextEditor){
+	let operationRange = editor!.selection.isEmpty ? GetRangeFullDocument(editor!.document) : new vscode.Range(editor.selection.start, editor.selection.end);
+	let text: string = editor!.document.getText(operationRange) ?? "";
+	
+	return {
+		operationRange,
+		text
+	}
 }
 
 function GetRangeFullDocument(document: vscode.TextDocument){
