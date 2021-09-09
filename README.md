d8e0c933c69fbd4cc5646cf89c1865f8811e6271
Óscar Míguez Alonso <omiguezalonso@gmail.com>
2021-09-09 17:56:03 +0200

    Documentation and style

--- README.md
+++ README.md
@@ -1,70 +1,13 @@
 # dsltoolstojson README
 
-This is the README for your extension "dsltoolstojson". After writing up a brief description, we recommend including the following sections.
+This is a tool that allows us to easily convert the format used in the DSLTool tests to represent data, to and from JSON.
 
-## Features
+The JSON view has the advantages of allowing you to have a wider view of the data (without being limited to one object at a time), and be able to more freely remove or copy data.
+This is is useful to inspect data when creating or reviewing a test case, cross reference data between two objects, or copy sections of the object to a different path of the same object.
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+## Usage
 
-For example if there is an image subfolder under your extension project workspace:
+To convert to JSON: On the Command Palette, use the command "Convert DSLTool output to JSON". If the Language Mode selected for the current document is JSON, it will also format it.
+To convert from JSON: On the Command Pallete, use the command "Convert JSON to DSLTool output".
 
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+If text is selected, only the selection will be transformed. Otherwise, the whole document will be transformed.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
-// Called when one of the commands is executed.
+/// <summary> Called when one of the commands is executed. </summary>
+/// <param name="context"> The VSCode extension context </param>
 export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Extension "dsltoolstojson" is now active!');
@@ -39,6 +40,9 @@ export function activate(context: vscode.ExtensionContext) {
 	}));
 }
 
+/// <summary> Gets the selected text and range, or the whole document and its range if nothing is selected. </summary>
+/// <param name="editor"> The active VSCode text editor. </param>
+/// <returns> The selected text and range, or the whole document and its range if nothing is selected. </returns>
 function GetTextToWorkWith(editor: vscode.TextEditor){
 	let operationRange = editor!.selection.isEmpty ? GetRangeFullDocument(editor!.document) : new vscode.Range(editor.selection.start, editor.selection.end);
 	let text: string = editor!.document.getText(operationRange) ?? "";
@@ -49,68 +53,92 @@ function GetTextToWorkWith(editor: vscode.TextEditor){
 	}
 }
 
+/// <summary> Gets the range of the full active document. </summary>
+/// <param name="document"> The active document. </param>
+/// <returns> The Range comprising the entire document. </returns>
 function GetRangeFullDocument(document: vscode.TextDocument){
 	const fullText = document.getText();
+	
 	return new vscode.Range(document.positionAt(0), document.positionAt(fullText.length))	
-
 }
+
+/// <summary> Converts text in the DSLTool format to JSON. </summary>
+/// <param name="text"> The text to transform. </param>
+/// <returns> The transformed JSON text. </returns>
 function DSLToolToJSON(text: string){
 	text = text.replace(/[\\]+"/gi, '"');
 	text = text.replace(/"\[\{/gi, "[{");
 	text = text.replace(/\}\]"/gi, "}]");
+
 	return text;
 }
 
+/// <summary> Converts JSON text to the DSLTools format. </summary>
+/// <param name="json"> The JSON text to transform. </param>
+/// <returns> The transformed text. </returns>
 function JSONToDSLTool(json: string) : string
 {
 	try{
 		let data = JSON.parse(json);
 		if (data == null){
 			console.log("Data was null");
+
 			return json;
 		}
 
 		console.log("Parse successful");
+
 		return DataToDSLTool(data);
 	}
 	catch(error){
 		console.log("Unexpected error", error);
+
 		return json;
 	}	
 }
 
-function DataToDSLTool(data: any, indexingLevel: number = 0) :string
+/// <summary> Serializes an object to the DSLTools format. </summary>
+/// <param name="data"> The object to serialized. </param>
+/// <param name="recursionLevel"> Allows the function to know how many backbars to add before a quote. </param>
+/// <returns> The serialized object. </returns>
+function DataToDSLTool(data: any, recursionLevel: number = 0) :string
 {
 	let output: string = "";
+
 	if (Array.isArray(data)){
 		output+="[";
+
 		for (let i = 0; i < data.length; i++){
-			output+=DataToDSLTool(data[i], indexingLevel)
+			output+=DataToDSLTool(data[i], recursionLevel)
 			if (i < data.length - 1)
 			{
 				output+=",";
 			}
 		}
+
 		output+="]"
 	}
-	else if (IsPrimitive(data)){
+	else if (IsPrimitive(data))
+	{
 		output += data;
 	}
-	else {
+	else
+	{
 		output += "{";
 		for (let property in data){
 			if (data.hasOwnProperty(property)) {
-				output += BuildQuote(indexingLevel);
+				output += BuildQuote(recursionLevel);
 				output += property;
-				output += BuildQuote(indexingLevel);
+				output += BuildQuote(recursionLevel);
 				output += ": ";	
-				output += BuildQuote(indexingLevel);
-				output += DataToDSLTool(data[property], indexingLevel + 1);
-				output += BuildQuote(indexingLevel);
+				output += BuildQuote(recursionLevel);
+				output += DataToDSLTool(data[property], recursionLevel + 1);
+				output += BuildQuote(recursionLevel);
 			}
 			
 			output += ","
 		}
+
 		output = output.substring(0, output.length - 1);
 		output += "}";		
 	}
@@ -118,13 +146,19 @@ function DataToDSLTool(data: any, indexingLevel: number = 0) :string
 	return output;
 }
 
-function BuildQuote(indexingLevel: number){
-	return "\\".repeat(indexingLevel) + '"';
+/// <summary> Creates a quote with as much backbars as needed. </summary>
+/// <param name="recursionLevel"> The current recursion level. </param>
+/// <returns> A string with the backbars and quote. </returns>
+function BuildQuote(recursionLevel: number){
+	return "\\".repeat(recursionLevel) + '"';
 }
 
+/// <summary> Checks if the given object is a primitive type. </summary>
+/// <param name="test"> The object to check. </param>
+/// <returns> True if it is a primitive, false otherwise. </returns>
 function IsPrimitive(test: any) {
     return test !== Object(test);
 }
 
-// This method is called when the extension is deactivated.
+// <summary> This method is called when the extension is deactivated. </summary>
 export function deactivate() {}
